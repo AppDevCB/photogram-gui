ff694a72c9ed54a4f1c9650954451fdd97b954ba
VladislavSCB <vladislav.samoylenko@chicagobooth.edu>
2022-11-03 21:32:31 +0000

    WIP class 4:30

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({ :username => :asc })
+
+    render({ :template => "user_templates/index.html.erb" })
+  end
+
+  def show
+    input_username = params.fetch("path_username")
+    User.where({ :username => input_username })
+
+    matching_username = User.where({ :username => input_username })
+    @the_user = matching_username.first
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show.html.erb" })
+    end
+  end
+end
